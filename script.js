af5c25a55b5be4d836645f2d14b4f5782fd35527
neil alabin <neiljoyce.alabin@yahoo.com>
2021-07-07 06:53:29 -0400

    Added mouseover event that changes the color of each block to cyan on mouse hover

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let container = document.querySelector('#container');
-
+let div;
 function makeRows(rows, colums) {
     for (i = 0; i < rows * colums; i++) {
-        let div = document.createElement('div');
+        div = document.createElement('div');
         div.classList.toggle('grid-class');
         container.appendChild(div);
     }
+
 }
 
 makeRows(16, 16);
+
+let gridClass = document.querySelectorAll('.grid-class');
+gridClass.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "cyan";
+    });
+})
