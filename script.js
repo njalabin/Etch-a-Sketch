a2cee9926ca8cf7e2a1bb434256a9898330ff04a
neil alabin <neiljoyce.alabin@yahoo.com>
2021-07-08 22:52:06 -0400

    Set grid display inside makeGrid function. Added remakeGrid Function that promps user for number of squares per side on click of button

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ let container = document.querySelector('#container');
 function makeGrid(squares) {
     for (i = 0; i < squares * squares; i++) {
         let div = document.createElement('div');
+        container.style.display = 'grid';
+        container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
         div.classList.toggle('grid-class');
         container.appendChild(div);
     }
-
 }
-
 makeGrid(16);
 
-let gridClass = document.querySelectorAll('.grid-class');
-gridClass.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "cyan";
-    });
-})
-
+// Reset button that turns backgroundColor to white on click
 let reset = document.querySelector('#reset');
 reset.addEventListener('click', resetGrid);
 function resetGrid() {
@@ -24,5 +19,26 @@ function resetGrid() {
     gridClass.forEach(div => div.style.backgroundColor = 'white');
 }
 
-// set container max width to 960px wide
-// set limit for user to input max 100x100 square divs
+// Remake grid based on number given by user on prompt
+let remake = document.querySelector('#reset');
+remake.addEventListener('click', remakeGrid);
+let userNumber;
+function remakeGrid (userNumber) {
+    userNumber = prompt('Give number per sides between 1 - 99');
+    if (userNumber === null || userNumber < 1 || userNumber > 100) {
+        userNumber = prompt('Give number per sides between 1 - 99');
+    } else if (userNumber > 1 || userNumber < 100) {
+        gridClass.forEach((div) => {
+            div.remove();
+        })
+        makeGrid(userNumber);
+    }
+}
+
+// Makes each square change color to cyan on mouseover
+let gridClass = document.querySelectorAll('.grid-class');
+gridClass.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "cyan";
+    });
+})
