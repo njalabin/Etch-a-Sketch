67da31c1b73f642ee44c17e1cf7b5dc5ee750920
neil alabin <neiljoyce.alabin@yahoo.com>
2021-07-10 01:13:06 -0400

    Cleaned up and removed unnecessary comments and/or code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let container = document.querySelector('#container');
+const container = document.querySelector('#container');
+let gridClass = document.querySelectorAll('.grid-class');
 function makeGrid(squares) {
     for (i = 0; i < squares * squares; i++) {
         let div = document.createElement('div');
@@ -12,7 +13,7 @@ function makeGrid(squares) {
 makeGrid(16);
 
 // Reset button that turns backgroundColor to white on click
-let reset = document.querySelector('#reset');
+const reset = document.querySelector('#reset');
 reset.addEventListener('click', resetGrid);
 function resetGrid() {
     gridClass = document.querySelectorAll('.grid-class');
@@ -20,12 +21,12 @@ function resetGrid() {
 }
 
 // Remake grid based on number given by user on prompt
-let remake = document.querySelector('#remake');
+const remake = document.querySelector('#remake');
 remake.addEventListener('click', remakeGrid);
 let userNumber;
 function remakeGrid (userNumber) {
     userNumber = prompt('Give number per sides between 1 - 99');
-    if (userNumber === null || userNumber < 1 || userNumber > 100) { // needs fixing
+    if (userNumber === null || userNumber < 1 || userNumber > 100) {
         userNumber = prompt('Give number per sides between 1 - 99');
     } else if (userNumber > 1 || userNumber < 100) {
         gridClass.forEach((div) => {
@@ -37,12 +38,10 @@ function remakeGrid (userNumber) {
     }
 }
 
-let gridClass = document.querySelectorAll('.grid-class');
-
 const blackBtn = document.querySelector('#black');
 blackBtn.addEventListener('click', turnBlack);
 function turnBlack() {
-    let gridClass = document.querySelectorAll('.grid-class');
+    gridClass = document.querySelectorAll('.grid-class');
     gridClass.forEach((div) => {
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = "black";
@@ -61,10 +60,6 @@ function turnCyan() {
     })
 };
 turnCyan();
-
-// RGB color on mouseover
-// Generate random number from 0 -255 x 3 for R, G, B
-//  
 
 const rgbBtn = document.querySelector('#rgb');
 rgbBtn.addEventListener('click', turnRGB);
